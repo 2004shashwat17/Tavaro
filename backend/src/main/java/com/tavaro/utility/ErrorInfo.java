89adf33bd32988014feee2f4846f0675514362ae
Shashwat Saxena <your-email@example.com>
2025-08-01 15:46:25 +0530

    push screenshot

---
+++ backend/src/main/java/com/tavaro/utility/ErrorInfo.java
@@ -0,0 +1,10 @@
+package com.tavaro.utility;
+
+import java.time.LocalDateTime;
+
+@Dat
+public class ErrorInfo {
+    private String errorMessage;
+    private Integer errorCode;
+    private LocalDateTime timeStamp;
+}
